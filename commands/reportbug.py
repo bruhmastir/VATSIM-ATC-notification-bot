0a2bc4f1663357dbeb28f9f43ff8bbc9ba5aedad
bruhmastir <ahmedathir19@gmail.com>
2025-03-05 20:42:21 +0000

    bug fix: thread name limit

--- commands/reportbug.py
+++ commands/reportbug.py
@@ -22,14 +22,16 @@ async def get_tag_by_name(channel: discord.ForumChannel, tag_name: str):
 
 async def handle(message, client):
     forum = await client.fetch_channel(FORUM_CHANNEL_ID)
-    tag = await get_tag_by_name(forum, "Reported through bot")
+    bot_report_tag = await get_tag_by_name(forum, "Reported through bot")
+    report_tag = await get_tag_by_name(forum, "Reported")
+    tags = [bot_report_tag, report_tag]
     report_as_list = message.content.split()[1:]
     if not report_as_list:
         await message.channel.send("Bug report failed. Please try again and provide a description of the bug you want to report.")
         return
     report = " ".join(report_as_list)
-    embed = discord.Embed(title=f"Report by {message.author.display_name}", description=message.content, color=discord.Color.red())
+    embed = discord.Embed(title=f"Report by {message.author.display_name}", description=f"Reporting user ID: {message.author.id}\nContent: {message.content}", color=discord.Color.red())
     embed.set_author(name=message.author.display_name, icon_url=message.author.avatar.url)
-    await forum.create_thread(name=f"{report[:-1] if len(report)<=100 else report[:99]}", embed=embed, reason="New bug report", applied_tags=[tag])
+    await forum.create_thread(name=f"{report[:-1] if len(report)<=100 else report[:99]}", embed=embed, reason="New bug report", applied_tags=tags)
     # await forum.create_thread(name=f"Report by {message.author.display_name}", embed=embed, reason="New bug report", applied_tags=[tag])
     # await forum.create_thread(name=f"Report by {message.author.display_name}", embed=embed, message=message.content, reason="New bug report", start_message=embed)
