009db4a60c00578767d3bec1aacfb60af0d42c80
bruhmastir <ahmedathir19@gmail.com>
2025-03-08 11:47:54 +0300

    find_prefix implementation

--- finder.py
+++ finder.py
@@ -1,6 +1,15 @@
+import config
+import logging
+
 bot_name = None
 
 def find_bot_name(client):
     global bot_name
     bot_name = str(client.user)
     # return bot_name
+
+def find_prefix(bot_name):
+    if not bot_name:
+        logging.error("Bot name not specified. Defaulting to config.PREFIX.")
+        return config.PREFIX
+    return config.PREFIX if not bot_name.lower().startswith("dev") else config.DEV_PREFIX
